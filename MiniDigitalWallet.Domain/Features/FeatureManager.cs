d520f7d80072395c7960088fd2c26cbcce40cadd
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-11-21 04:02:29 +0630

    feat: add FeatureManager.cs

--- MiniDigitalWallet.Domain/Features/FeatureManager.cs
+++ MiniDigitalWallet.Domain/Features/FeatureManager.cs
@@ -1,9 +1,16 @@
-﻿namespace MiniDigitalWallet.Domain.Features;
+﻿using MiniDigitalWallet.Domain.Features.ChangePin;
+using MiniDigitalWallet.Domain.Features.Profile;
+using MiniDigitalWallet.Domain.Features.Transfer;
+
+namespace MiniDigitalWallet.Domain.Features;
 
 public static class FeatureManager
 {
     public static void AddDomain(this IServiceCollection services)
     {
         services.AddScoped<RegisterService>();
+        services.AddScoped<ProfileService>();
+        services.AddScoped<ChangePinService>();
+        services.AddScoped<TransferService>();
     }
 }
