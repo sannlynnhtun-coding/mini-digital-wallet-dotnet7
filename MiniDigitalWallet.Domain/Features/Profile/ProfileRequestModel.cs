41218b1178a0443856429ff1e45dd546f6e9329d
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-11-21 04:01:36 +0630

    feat: Profile

---
+++ MiniDigitalWallet.Domain/Features/Profile/ProfileRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MiniDigitalWallet.Domain.Features.Profile;
+
+public abstract class ProfileRequestModel
+{
+    public int UserId { get; set; }
+    public string MobileNumber { get; set; } = null!;
+    public string UserName { get; set; } = null!;
+    public string PinCode { get; set; } = null!;
+    public decimal? Balance { get; set; }
+    public string? Status { get; set; }
+}
---
+++ MiniDigitalWallet.Domain/Features/Profile/ProfileService.cs
@@ -0,0 +1,64 @@
+﻿namespace MiniDigitalWallet.Domain.Features.Profile;
+
+public class ProfileService
+{
+    private readonly AppDbContext _db;
+
+    public ProfileService(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<Result<ProfileResponseModel>> UpdateProfileAsync(ProfileRequestModel requestModel)
+    {
+        Result<ProfileResponseModel> model;
+        try
+        {
+            var item = await _db.TblWalletUsers.FirstOrDefaultAsync(u => u.UserId == requestModel.UserId);
+            if (item == null)
+                return Result<ProfileResponseModel>.ValidationError("User not found.");
+
+            #region Validation
+
+            var result = await new ProfileRequestModelValidator()
+                .ValidateModelAsync<ProfileRequestModel, ProfileResponseModel>(requestModel);
+            if (result.IsValidationError)
+            {
+                model = result;
+                goto Response;
+            }
+
+            #endregion
+
+            #region Update Profile
+
+            item.UserName = requestModel.UserName;
+            item.MobileNumber = requestModel.MobileNumber;
+            item.Balance = requestModel.Balance;
+            item.Status = requestModel.Status;
+
+            _db.Entry(item).State = EntityState.Modified;
+            await _db.SaveAndDetachAsync();
+            
+            var responseModel = new ProfileResponseModel
+            {
+                UserId = item.UserId,
+                UserName = item.UserName,
+                MobileNumber = item.MobileNumber,
+                Balance = item.Balance,
+                Status = item.Status
+            };
+
+            model = Result<ProfileResponseModel>.Success(responseModel, "Profile updated successfully.");
+
+            #endregion
+        }
+        catch (Exception ex)
+        {
+            model = Result<ProfileResponseModel>.SystemError(ex.Message);
+        }
+
+        Response:
+        return model;
+    }
+}
